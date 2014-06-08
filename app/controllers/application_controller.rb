10d3f963a61ac7235c971f5af97668931571df6f
Wil Helser <wil@wilhelser.com>
2014-06-08 11:53:53 -0500

    add permitted attributes to users from application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,22 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resource)
-    resource.site
+    if resource.instance_of?(User)
+      resource.site
+    else
+      super
+    end
   end
 
   def after_sign_out_path_for(resource_or_scope)
     root_path
+  end
+
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :password, :password_confirmation, :first_name, :last_name, :member_id, :login_code, :email_updates) }
   end
 end
