c8c912d6d1e5820bd1562ff0629cfea627b36dba
Wil Helser <wil@wilhelser.com>
2014-06-13 15:21:51 -0500

    fix home site page benefits

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -34,7 +34,7 @@ class SitesController < ApplicationController
     @category = params[:search][:category]
     @distance = params[:search][:distance] ||= 25
     # rss = HTTParty.get("http://api.entertainment.com/AtomServer3/feeds/offers?uuid=1401914342909&location=#{@zip_code}&distance=25&category=#{@category}", basic_auth: { username: "INFO@SISK.COM", password: "T1aPw4SjF" })
-
+    # @feed = RSS::Parser.parse(rss, false)
     @feed = Atom::Feed.load_feed(URI.parse("http://api.entertainment.com/AtomServer3/feeds/offers?uuid=1401914342909&location=#{@zip_code}&distance=#{@distance}&category=#{@category}"), :user => 'INFO@SISK.COM', :pass => 'T1aPw4SjF')
     # @feed = Feedjira::Feed.fetch_and_parse("http://api.entertainment.com/AtomServer3/feeds/offers?uuid=1401914342909&location=#{@zip_code}&distance=#{@distance}&category=#{@category}", :http_authentication => ["INFO@SISK.COM", "T1aPw4SjF"])
     # @response = RSS::Parser.parse(rss, false)
