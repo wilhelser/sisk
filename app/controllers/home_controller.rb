c0259e6a1240bf66dd3af842fef67e3bbc68b79d
Wil Helser <wil@wilhelser.com>
2014-06-13 00:37:27 -0500

    edit account page update

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,10 +2,7 @@ class HomeController < ApplicationController
   layout 'sites', only: [:about]
 
   def index
-    if params[:site_code]
-      @site_code = params[:site_code]
-      send_to_site(@site_code)
-    end
+    @body_class = 'home'
   end
 
   def about
@@ -16,14 +13,4 @@ class HomeController < ApplicationController
     @page_title = "Member Benefit Usage Instructions"
   end
 
-  private
-
-  def send_to_site(site_code)
-    @site = Site.find_by_site_code(site_code)
-    if @site
-      redirect_to @site
-    else
-      render action: 'index', notice: "Invalid Site code"
-    end
-  end
 end
