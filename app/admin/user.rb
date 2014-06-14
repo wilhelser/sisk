71ae682f18e8a274ecc4788a5d38a8a482f48127
Wil Helser <wil@wilhelser.com>
2014-06-14 15:44:12 -0500

    add ability to change users site from admin panel

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -1,17 +1,33 @@
 ActiveAdmin.register User do
+  permit_params :first_name, :last_name, :email, :password, :password_confirmation, :current_password, :first_name, :last_name, :email_updates, :site_id
 
-  
-  # See permitted parameters documentation:
-  # https://github.com/gregbell/active_admin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # permit_params :list, :of, :attributes, :on, :model
-  #
-  # or
-  #
-  # permit_params do
-  #  permitted = [:permitted, :attributes]
-  #  permitted << :other if resource.something?
-  #  permitted
-  # end
-  
+  # Index
+  index do
+    selectable_column
+    column :id
+    column :first_name
+    column :last_name
+    column :site_id
+    column :email_updates
+    actions
+  end
+
+  # Form
+  form do |f|
+    f.inputs "User Details" do
+      f.input :first_name
+      f.input :last_name
+      f.input :first_name
+      f.input :last_name
+      f.input :email
+      f.input :password
+      f.input :password_confirmation
+      f.input :first_name
+      f.input :last_name
+      f.input :email_updates
+      f.input :site_id, as: :select, collection: Site.all
+    end
+    f.actions
+  end
+
 end
