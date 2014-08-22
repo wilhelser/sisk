493d8ee1fccfa9e877ace7c805a6812bd8ff18ff
Wil Helser <wil@wilhelser.com>
2014-08-22 13:21:55 -0500

    update

--- db/migrate/20140820153914_add_logo_uid_to_custom_sites.rb
+++
@@ -1,5 +0,0 @@
-class AddLogoUidToCustomSites < ActiveRecord::Migration
-  def change
-    add_column :custom_sites, :logo_uid, :string, limit: 80
-  end
-end
--- db/migrate/20140822172721_remove_logo_uid_from_custom_sites.rb
+++
@@ -1,5 +0,0 @@
-class RemoveLogoUidFromCustomSites < ActiveRecord::Migration
-  def change
-    remove_column :custom_sites, :logo_uid
-  end
-end
