336e1cc87d4658d5616596045240fd99e80a853f
Wil Helser <wil@wilhelser.com>
2014-09-19 12:01:11 -0500

    update to admin template

--- app/controllers/custom_sites_controller.rb
+++ app/controllers/custom_sites_controller.rb
@@ -45,7 +45,7 @@ class CustomSitesController < ApplicationController
   end
 
   def custom_site_params
-    params.require(:site).permit(:name, :url, :company_name, :city, :state, :zip_code, :primary_color, :secondary_color, :link_color, :login_code, :link_color_hover, :club_name, :bank_website, :template, :account_number, :logo, :content_section)
+    params.require(:site).permit(:name, :url, :company_name, :city, :state, :zip_code, :primary_color, :secondary_color, :link_color, :login_code, :link_color_hover, :club_name, :bank_website, :template, :account_number, :logo, :content_section_id)
   end
 
   private
