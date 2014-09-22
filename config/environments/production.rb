11cd8fb3484b0c673d772e73409737d0eb63e720
Wil Helser <wil@wilhelser.com>
2014-09-22 12:28:10 -0500

    update to production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,7 +69,7 @@ Sisk::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  config.assets.precompile += %w( template1.css template2.css template3.css )
+  config.assets.precompile += %w( template1/style.css template2/style.css template3/style.css )
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
