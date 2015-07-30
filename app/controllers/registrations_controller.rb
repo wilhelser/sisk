f0950c2315706a507ddc1ae2dee30f912e6e5bd5
Wil Helser <wil@wilh.me>
2015-07-30 13:20:45 -0500

    finally fixed that shit

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -15,4 +15,8 @@ class RegistrationsController < Devise::RegistrationsController
   def update_resource(resource, params)
     resource.update_without_password(params)
   end
+
+  def after_update_path_for(resource)
+    edit_user_registration_path(resource)
+  end
 end
